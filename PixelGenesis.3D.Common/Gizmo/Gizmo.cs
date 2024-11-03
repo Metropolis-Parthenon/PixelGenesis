064aa6da22c38d8e57d181d1b79ff6c7d42524ca
Alejandro Guardiola Muñoz <aleguardiolam@gmail.com>
2024-11-03 02:47:53 -0500

    Add Gizmo class and namespace for future shape functions
    Introduced a new static class `Gizmo` within the `PixelGenesis._3D.Common.Gizmo` namespace. Added several using directives for common .NET namespaces: `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`. Included a TODO comment for future implementation of shape functions in the editor.

---
+++ PixelGenesis.3D.Common/Gizmo/Gizmo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelGenesis._3D.Common.Gizmo;
+
+public static class Gizmo
+{
+    // TODO: Functions to write shapes for the editor    
+}
